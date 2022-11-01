0746c8dac0861f2ef9924342aa3eec98f8d6845b
tungwingho <tungwingho@outlook.com>
2022-11-01 20:20:25 +0800

    feat(login): build the login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css';
+import Login from './pages/login';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Exsim
-      </h1>
+      <Login />
     </div>
   );
 }
