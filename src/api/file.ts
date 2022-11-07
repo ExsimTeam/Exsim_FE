5afd28f07a0466ea141759492630d7b04f357f52
XendXBY <xendxby@163.com>
2022-11-07 22:39:06 +0800

    feat(file): 完善了file页面的功能。

--- src/api/file.ts
+++ src/api/file.ts
@@ -20,3 +20,36 @@ export const useFileList = () => {
     url: 'file/getFileList'
   })
 }
+
+export const useNewFile = () => {
+  return useAxios<{
+    fileName: string,
+    property: number,
+    description?: string
+  }, {}>({
+    method: 'POST',
+    url: 'file/newFile'
+  })
+}
+
+export const useDeleteFile = () => {
+  return useAxios<{
+    fileId: number
+  }, {}>({
+    method: 'POST',
+    url: 'file/deleteFile'
+  })
+}
+
+export const useShareFile = () => {
+  return useAxios<{
+    shareToEmail: string,
+    permission: number,
+    fileId: number
+  }, {
+    username: string
+  }>({
+    method: 'POST',
+    url: 'file/shareFile'
+  })
+}
