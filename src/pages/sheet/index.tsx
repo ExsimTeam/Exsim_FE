c7aecd1642b671bd9201c4213581dd9802613d6f
tungwingho <tungwingho@outlook.com>
2022-11-08 10:31:16 +0800

    feat(websocket): online collaborate

--- src/pages/sheet/index.tsx
+++ src/pages/sheet/index.tsx
@@ -2,7 +2,7 @@ import { message } from "antd";
 import { Component, useEffect } from "react";
 import { NavigateFunction, useNavigate, useParams } from "react-router-dom";
 import { getFileBody, openFile } from "src/api/file";
-import { isNumeric, toName } from "src/common/utils";
+import { isNumeric, toJSON, toName } from "src/common/utils";
 import SpreadSheet, { CellData } from "src/component/SpreadSheet";
 import { Loading } from "src/utils/lazyLoad";
 
@@ -17,7 +17,7 @@ class SheetMain extends Component<{ sheetId: string | undefined, navigate: Navig
   sheet: SpreadSheet | null = null;
   ws: WebSocket | null = null
   utoken: string = ''
-  sheetId: number = 0
+  fileId: number = 0
 
   constructor(props: any) {
     super(props);
@@ -29,7 +29,7 @@ class SheetMain extends Component<{ sheetId: string | undefined, navigate: Navig
 
     const sheetId = this.props.sheetId
     if (isNumeric(sheetId)) {
-      this.sheetId = parseInt(sheetId!)
+      this.fileId = parseInt(sheetId!)
       this.openWebsocket()
     } else {
       this.props.navigate('/')
@@ -41,16 +41,14 @@ class SheetMain extends Component<{ sheetId: string | undefined, navigate: Navig
       return
     }
     // Get the utoken of file
-    openFile(this.sheetId)
+    openFile(this.fileId)
       .then(response => {
         if (response.code === 1) {
           this.utoken = response.data.utoken
           this.ws = new WebSocket(`ws://45.76.96.123:8888/api/edit?utoken=${this.utoken}`)
 
           this.ws.onopen = (e) => {
-            this.setState({
-              isLoading: false
-            })
+            console.log('WebSocket connection established')
           }
 
           this.ws.onmessage = (e) => {
@@ -61,9 +59,63 @@ class SheetMain extends Component<{ sheetId: string | undefined, navigate: Navig
               message.error('连接出错')
             }
             console.log(data)
-            switch (data.code) {
 
+            if (data.code === 1) {
+              switch (data.opcode) {
+                case -1: {
+                  // Server message
+                  if (this.state.isLoading)
+                    this.setState({
+                      isLoading: false
+                    })
+                  break
+                }
+                case 0: {
+                  // Modify a cell
+                  let opt = toJSON(data.data, () => message.error('消息出错'))
+                  console.log(opt)
+                  this.setData(opt.col, opt.row, { value: opt.value, format: opt.format }, true)
+                  this.sheet?.setState({})
+                  break
+                }
+                case 1: {
+
+                  break;
+                }
+                case 2: {
+                  break
+                }
+                case 3: {
+                  break
+                }
+                case 4: {
+                  break
+                }
+              }
+            } else if (data.code === 400) {
+              message.error('消息有误')
+            } else if (data.code === 401) {
+              message.error('身份认证失败')
+              setTimeout(() => {
+                this.props.navigate('/')
+              }, 1000)
+            } else {
+              message.error('未知错误')
             }
+          }
+
+          this.ws.onclose = () => {
+            console.log('WebSocket closed')
+            this.setState({
+              isLoading: true
+            })
+            this.openWebsocket()
+            message.info('连接丢失，尝试重新建立')
+          }
+
+          this.ws.onerror = (e) => {
+            message.error('连接出错')
+            this.openWebsocket()
           }
         } else if (response.code === 100) {
           message.error('文件不存在')
@@ -79,6 +131,22 @@ class SheetMain extends Component<{ sheetId: string | undefined, navigate: Navig
       })
   }
 
+  modifyCell = (sheetId: number, x: number, y: number, s: CellData) => {
+    let data = JSON.stringify({
+      opcode: 0,
+      data: {
+        sheetId: sheetId,
+        row: y,
+        col: x,
+        value: s.value,
+        format: s.format
+      }
+    })
+    console.log(s.format)
+    console.log(data)
+    this.ws?.send(data)
+  }
+
   getData = (x: number, y: number) => {
     if (this.data[x - 1] !== undefined) {
       if (this.data[x - 1][y - 1] !== undefined) {
@@ -89,11 +157,12 @@ class SheetMain extends Component<{ sheetId: string | undefined, navigate: Navig
     return null;
   }
 
-  setData = (x: number, y: number, s: any) => {
+  setData = (x: number, y: number, s: any, websocket = false) => {
     if (this.data[x - 1] === undefined) {
       this.data[x - 1] = [];
     }
     this.data[x - 1][y - 1] = s;
+    if (!websocket) this.modifyCell(0, x, y, s)
   }
 
   // return if horizontal page of pageIndex is ready
@@ -102,7 +171,7 @@ class SheetMain extends Component<{ sheetId: string | undefined, navigate: Navig
       this.status[pageIndex] = false;
 
       // Request data here
-      getFileBody(this.sheetId, pageIndex, 0)
+      getFileBody(this.fileId, pageIndex, 0)
         .then(response => {
           if (response.code === 1) {
             this.status[pageIndex] = true
@@ -120,17 +189,11 @@ class SheetMain extends Component<{ sheetId: string | undefined, navigate: Navig
                   let v = row[key]
 
                   // Try to parse format into JSON
-                  let format
-                  try {
-                    format = JSON.parse(v.format)
-                  } catch (error) {
-                    format = {}
-                    message.warn(`单元格${toName(colNum)}${rowNum}格式错误`)
-                  }
+                  let format = toJSON(v.format, () => message.warn(`单元格${toName(colNum)}${rowNum}格式错误`))
                   this.setData(colNum, rowNum, {
                     value: v.value,
-                    format: format
-                  })
+                    format: format ? format : {}
+                  }, true)
                 }
               }
             }
